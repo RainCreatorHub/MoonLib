4e0d8bb1510bdb4cf958a06c19c2a57323c434cd
RainCreatorHub <rain.hub.creator@gmail.com>
2025-05-28 01:29:04 -0300

    Update OrionLibV2.lua

--- OrionLibV2.lua
+++ OrionLibV2.lua
@@ -1396,7 +1396,7 @@ function OrionLibV2:MakeWindow(Info)
                 local buttonY = DropdownInner.AbsolutePosition.Y
                 DropdownHolderCanvas.Position = UDim2.fromOffset(
                     DropdownInner.AbsolutePosition.X - window.AbsolutePosition.X,
-                    buttonY - canvasHeight + 10 -- 5 studs acima + 10 studs para baixo
+                    buttonY - canvasHeight + 20 -- 10 studs abaixo + mais 10 studs abaixo
                 )
             end
 
