49d033912f47936cb666fe85de9ee55f34b7efc1
RainCreatorHub <rain.hub.creator@gmail.com>
2025-07-30 14:54:02 -0300

    Update MoonLib.lua

--- MoonLib.lua
+++ MoonLib.lua
@@ -306,7 +306,7 @@ function TabFunctions:AddLabel(info)
         tempNameLabel:Destroy()
 
         local totalNameHeight = #nameLines * nameLineHeight
-        local yOffset = -(totalNameHeight / 2) + 3
+        local yOffset = -(totalNameHeight / 2) + 5
         for _, line in ipairs(nameLines) do
             local nameLabel = createTextLabel(line, Enum.Font.GothamBold, 14, Color3.fromRGB(255, 255, 255), UDim2.new(0, 5, 0.5, yOffset), labelContainer, -20)
             nameLabel.Size = UDim2.new(1, -20, 0, nameLineHeight)
